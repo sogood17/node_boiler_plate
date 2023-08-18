d9a4fb5a9b2aed340997aa0cf910f491b08c676b
sogood17 <sogoodpiano@gmail.com>
2023-08-18 12:24:51 +0900

    mongodb, mongoose install

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = required('express')
 const app = express()
 const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://gayoungkim:abcd1234@cluster0.vx4znsr.mongodb.net/?retryWrites=true&w=majority`, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=> console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
