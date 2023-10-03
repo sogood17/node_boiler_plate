4cb4dca311b725e86f0d804e8a0121f43d9e314c
sogood17 <sogoodpiano@gmail.com>
2023-10-03 13:41:37 +0900

    로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,31 @@
 import {useEffect} from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+  const navigate = useNavigate();
+  
+
   useEffect(()=>{
     axios.get("/api/hello")
     .then(response => console.log(response.data))
   }, [])
   
+  const onClickHandler = () => {
+    axios.get("/api/users/logout")
+    .then(response => {
+      if (response.data.success) {
+        navigate('/login')
+      } else {
+        alert('로그아웃 하는 데 실패하였습니다.')
+      }
+    })
+  }
 
   return (
     <div>
-      랜딩페이지
+      <h2>랜딩페이지</h2>
+      <button onClick={onClickHandler}>logout</button>
     </div>
   )
 }
